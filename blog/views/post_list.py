00069c5f4042357c3e3f9cd5f9f0f78e0fae1d2b
Yousef Al-Hadhrami <yhadhr@gmail.com>
2025-02-14 14:41:11 +0100

    RTL support and imported i18n templates

--- blog/views/post_list.py
+++ blog/views/post_list.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from ..models import Post
+from django.utils.translation import get_language
 
 def post_list(request):
+    """Render the blog post list with language context."""
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, "blog/post_list.html", {"posts": posts, "LANGUAGE_CODE": get_language()})
