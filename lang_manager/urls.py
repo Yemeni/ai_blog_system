170aa53cea59b7a7d76de9fecdc7d655d96d8607
Yousef Al-Hadhrami <yhadhr@gmail.com>
2025-02-16 23:21:28 +0100

    Removed extra app name warning

--- lang_manager/urls.py
+++ lang_manager/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from django.views.i18n import set_language
 
-app_name = "lang_manager" 
-
-
 urlpatterns = [
     path("set_language/", set_language, name="set_language"),  # Language switcher support
 ]
