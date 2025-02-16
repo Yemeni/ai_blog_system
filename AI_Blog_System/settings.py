8fc82afff707e6187f0cbcd606e83903377227d0
Yousef Al-Hadhrami <yhadhr@gmail.com>
2025-02-16 23:22:00 +0100

    added core to main urls and to apps

--- AI_Blog_System/settings.py
+++ AI_Blog_System/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "blog",
     "parler",
     "lang_manager",
+    'core',
 ]
 
 MIDDLEWARE = [
--- AI_Blog_System/urls.py
+++ AI_Blog_System/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
 
 # API and Rosetta should NOT be affected by i18n
 urlpatterns += [
-    path("api/", include("lang_manager.urls")),  # API should remain language-neutral
-    path("rosetta/", include("rosetta.urls")),  # Rosetta should remain language-neutral
+    path("lang_manager/", include("lang_manager.urls")),  
+    path("lang_manager/", include("lang_manager.admin_urls")),  
+    path("rosetta/", include("rosetta.urls")),  
+    path("core/", include("core.urls")), 
 ]
 
 # Apply language prefix to Django Admin and lang_manager
 urlpatterns += i18n_patterns(
     path("admin/", admin.site.urls),
-    path("lang_manager/", include("lang_manager.admin_urls")),  # Ensure admin actions respect i18n
-    path("lang_manager/", include("lang_manager.urls")),  # Ensure admin actions respect i18n
     path("", include("blog.urls")),
 )
